3621e001274e2369ed19e05776c8ad605b7ac80c
Macek <macektarant@gmail.com>
2025-10-27 20:11:51 +0100

    barvy + splash a ikona

--- components/themed-view.tsx
+++ components/themed-view.tsx
@@ -1,14 +1,15 @@
-import { View, type ViewProps } from 'react-native';
-
 import { useThemeColor } from '@/hooks/use-theme-color';
+import React from 'react';
+import { View, ViewProps } from 'react-native';
 
-export type ThemedViewProps = ViewProps & {
-  lightColor?: string;
-  darkColor?: string;
-};
+export function ThemedView({
+  style,
+  lightColor,
+  darkColor,
+  type = 'background', // 👈 přidáme typ
+  ...otherProps
+}: ViewProps & { lightColor?: string; darkColor?: string; type?: 'background' | 'surface' }) {
+  const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, type)
 
-export function ThemedView({ style, lightColor, darkColor, ...otherProps }: ThemedViewProps) {
-  const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
-
-  return <View style={[{ backgroundColor }, style]} {...otherProps} />;
+  return <View style={[{ backgroundColor }, style]} {...otherProps} />
 }
